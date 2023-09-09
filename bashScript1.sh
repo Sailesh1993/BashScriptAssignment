001e17652b36ee1f8184d903382ffa8a84811e0c
Sailesh1993 <karki.saileshnep@gmail.com>
2023-09-09 10:55:36 +0300

    Initialize new commit and added bashScript1 for Assignment1 with output

---
+++ bashScript1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# use control flow to check the number of arguments
+if [ "$#" == 0 ];
+then
+    echo "Error: File path is missing"
+    exit 1
+elif [ "$#" == 1 ];
+then 
+    cd $1
+    git shortlog -sn > ../output.txt
+else
+    cd $1
+    for name in "$@" ; do
+        git shortlog -sn | grep "$name" >> ../output.txt
+    done
+fi
